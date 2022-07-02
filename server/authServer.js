4802a0a8642e11c0c874f77c777dbe2069748c80
Kaustav Mukhopadhyay <mu.kaustav@gmail.com>
2022-07-02 16:38:42 +0530

    add favicon

--- server/authServer.js
+++ server/authServer.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
+const path = require('path')
 const cors = require('cors')
 const express = require('express')
+const favicon = require('serve-favicon')
 const authRoute = require('./routes/auth')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(favicon(path.join(__dirname, './assets', 'favicon.png')))
 
 app.get('/', (req, res) => {
     res.json({
